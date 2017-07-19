7058d931d282a33db9b687b40908bb2c9f7a869b
shosato <shosato@MBP.local>
2017-07-19 21:53:16 +0900

    [update]試しに解像度を画面に表示する

--- app/src/main/java/com/example/shosato/dpchecker/MainActivity.kt
+++ app/src/main/java/com/example/shosato/dpchecker/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.shosato.dpchecker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val displayMetrics: DisplayMetrics = resources.displayMetrics
+        val contentText: TextView = findViewById(R.id.dpContent) as TextView
+        contentText.text = displayMetrics.densityDpi.toString()
     }
 }
