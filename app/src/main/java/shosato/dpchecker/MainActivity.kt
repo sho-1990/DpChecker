67f73f4caafd7f7202f0c4f834963ee93ba9b745
sh.sato <sh.sato@jwork.co.jp>
2017-07-20 15:57:21 +0900

    [update]AdMobViewのライフサイクル管理を行う

--- app/src/main/java/shosato/dpchecker/MainActivity.kt
+++ app/src/main/java/shosato/dpchecker/MainActivity.kt
@@ -6,13 +6,19 @@ import android.util.DisplayMetrics
 import android.widget.TextView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import shosato.dpchecker.logic.DpStringGenerator
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdView :AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this, resources.getString(R.string.banner_ad_unit_id))
+        loadAd()
 
         val displayMetrics: DisplayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
@@ -25,13 +31,26 @@ class MainActivity : AppCompatActivity() {
         contentText.text = dpContent
         val contentNumText: TextView = findViewById(R.id.dpContentNum) as TextView
         contentNumText.text = dp.toString()
+    }
 
-        loadAd()
+    override fun onResume() {
+        super.onResume()
+        mAdView.resume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mAdView.destroy()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mAdView.pause()
     }
 
     private fun loadAd() {
-        val ad : AdView = findViewById(R.id.adView) as AdView
+        mAdView = findViewById(R.id.adView) as AdView
         val adRequest :AdRequest = AdRequest.Builder().build()
-        ad.loadAd(adRequest)
+        mAdView.loadAd(adRequest)
     }
 }
